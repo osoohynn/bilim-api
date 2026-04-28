4cafceb90f97f12e73c650eda3e8c3064cfeace3
osoohynn <tbr335@naver.com>
2026-04-28 10:37:54 +0900

    initial commit

---
+++ src/main/java/com/dgsw/cobookapi/CobookApiApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.cobookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobookApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CobookApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgsw/cobookapi/CobookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.cobookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobookApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
